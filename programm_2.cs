af16e18281016d0d73868be7d0791150dcde8847
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2022-11-04 18:16:10 +0300

    Create programm_2.cs

---
+++ programm_2.cs
@@ -0,0 +1,44 @@
+using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            // однострочный комент
+            // @allelleo
+            
+            /*
+             * 
+             * м
+             * н
+             * о
+             * г
+             * о
+             * м
+             * т
+             * р
+             * о
+             * ч
+             * н
+             * ы
+             * й
+             *
+             * к
+             * о
+             * м
+             * е
+             * н
+             * т
+             * а
+             * р
+             * и
+             * й
+             * 
+             */
+        }
+    }
+}
