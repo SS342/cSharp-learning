3081cdcafbcefe31cc0033c4137e925274ebb8ef
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2022-11-04 18:40:55 +0300

    Create programm_3.cs

---
+++ programm_3.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+namespace ConsoleApp1
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello world!");
+            int a; // обьявили переменную
+            a = 10; // присвоили ей значение
+
+            int b = 15; // или сразу так
+
+            char sumbol = 'q'; // символ - одиночный кавычки, на двойные ругается
+            float pi = 3.14F; // число пи, буква F на конце флоат - обязательна
+
+            int x = 5, y = 6, z = 7; // можно объявлять переменные сразу так
+
+        }
+    }
+}
