23c511c818b5d42d65fefbfd69ba23e00add772e
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2022-11-05 12:10:53 +0300

    Create programm13.cs

---
+++ programm13.cs
@@ -0,0 +1,17 @@
+using System;
+namespace ConsoleApplication1
+{    
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Hello!");
+            } while (Console.ReadLine() != "q");
+
+        }
+    }
+}
