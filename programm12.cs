9a194c980d080befff22263262bb60572a4b0ea7
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2022-11-05 12:00:56 +0300

    Create programm12.cs

---
+++ programm12.cs
@@ -0,0 +1,19 @@
+using System;
+namespace ConsoleApplication1
+{    
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 100)
+            {
+                i++;
+                Console.WriteLine("{0} %", i);
+            }
+
+        }
+    }
+}
